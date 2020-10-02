ce973889afdc7bc45649e6e40f319ed45806ce56
Roksana Dzioba <roksanadzioba@gmail.com>
2020-10-02 23:27:16 +0200

    todo app with lit-element

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './styles.css';
+import './views/todo-view.js';
+import '@vaadin/vaadin-text-field';
+import '@vaadin/vaadin-button';
+import '@vaadin/vaadin-checkbox';
+import '@vaadin/vaadin-radio-button/vaadin-radio-button';
+import '@vaadin/vaadin-radio-button/vaadin-radio-group';
